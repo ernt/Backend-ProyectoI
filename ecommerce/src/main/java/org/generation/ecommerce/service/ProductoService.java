7870ed9bb2e64e7cc1cc1ed30565a322f288301c
ARTR <trejoramon141295@gmail.com>
2023-05-12 18:00:05 -0600

    Correcion de paquete mal ubicado

--- ecommerce/src/main/java/org/generation/ecommerce/service/ProductoService.java
+++ ecommerce/src/main/java/org/generation/ecommerce/service/ProductoService.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.generation.ecommerce.model.Producto;
-import org.generation.ecommerce.service.repository.ProductoRepository;
+import org.generation.ecommerce.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
