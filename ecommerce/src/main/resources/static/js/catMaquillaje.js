b521373edb41f4ee6187aba2a07049d0964fd0e1
juan pablo <chito.pepsi@gmail.com>
2023-05-19 01:20:04 -0600

    productos y categorias con fetch

--- ecommerce/src/main/resources/static/js/catMaquillaje.js
+++ ecommerce/src/main/resources/static/js/catMaquillaje.js
@@ -1,11 +1,11 @@
 let contenedorTarjetas = document.getElementById("contenedorTarjetas");
 
-window.addEventListener("load", function () {
-  if (localStorage.getItem("datos") !== null) {
-    datos = JSON.parse(localStorage.getItem("datos"));
-
-    datos
-      .filter((item) => item.categoria === "maquillaje")
+fetch("http://127.0.0.1:8080/api/productos/")
+  .then((response) => response.json())
+  .then((products) => {
+    // Maneja la lista de productos
+    products
+      .filter((item) => item.categorias_id === "8")
       .forEach((cargado) => {
         let card = `
         <div id="${cargado.id}" class="card m-3 col-sm-4 col-md-4 col-lg-3 col-xl-3 ">
@@ -23,7 +23,7 @@ window.addEventListener("load", function () {
             ${cargado.nombre}
             </h3>
             <span>
-            Precio: <strong>${cargado.precio}</strong>
+            Precio: <strong >${cargado.precio}</strong>
             </span>
             <p>
             ${cargado.descripcion}
@@ -37,56 +37,27 @@ window.addEventListener("load", function () {
 
         contenedorTarjetas.insertAdjacentHTML("beforeend", card);
       });
-  }
-  let buttons = document.querySelectorAll(".popover-btn");
-  let popovers = document.querySelectorAll(".popover");
+    let buttons = document.querySelectorAll(".popover-btn");
+    let popovers = document.querySelectorAll(".popover");
 
-  buttons.forEach(function (button, index) {
-    let popover = popovers[index];
-    button.addEventListener("click", function () {
-      popover.style.display = "block";
-    });
-  });
-
-  document.addEventListener("click", function (event) {
     buttons.forEach(function (button, index) {
       let popover = popovers[index];
-      if (!button.contains(event.target) && !popover.contains(event.target)) {
-        popover.style.display = "none";
-      }
+      button.addEventListener("click", function () {
+        popover.style.display = "block";
+      });
     });
-  });
-});
-articulos
-  .filter((item) => item.categoria === "maquillaje")
-  .forEach((cargado) => {
-    let card = `
-  <div id="${cargado.id}" class="card m-3 col-sm-4 col-md-4 col-lg-3 col-xl-3 ">
-  <img src="${cargado.imagen}" alt="img" class="img-fluid fixed-image cardImage">
-    <div class="card-body">
-      <h5 class="card-title">${cargado.nombre}</h5>
-      <p class="card-price">Precio: ${cargado.precio}</p>
-      <div>
-      <div>
-      <p class="card-text">${cargado.descripcion}</p>
-      <button class="popover-btn">Leer más </button>
-      <button  onclick="agregarProducto(event)" class="btn carritoBoton"><i class="bi bi-cart-plus fs-4"></i></button>
-      <section class="popover">
-      <h3>
-      ${cargado.nombre}
-      </h3>
-      <span>
-      Precio: <strong>${cargado.precio}</strong>
-      </span>
-      <p>
-      ${cargado.descripcion}
-      </p>
-      <button onclick="eliminarPoper(event)" class="" id="btnPoper"><i class="bi bi-x-circle fs-5"></i></button>
-      </section>
-      </div>
-    </div>
-  </div>
-`;
 
-    contenedorTarjetas.insertAdjacentHTML("beforeend", card);
+    document.addEventListener("click", function (event) {
+      buttons.forEach(function (button, index) {
+        let popover = popovers[index];
+        if (!button.contains(event.target) && !popover.contains(event.target)) {
+          popover.style.display = "none";
+        }
+      });
+    });
+    // Realiza acciones adicionales con la lista de productos, como mostrarlos en la interfaz de usuario
+  })
+  .catch((error) => {
+    // Maneja errores de la solicitud
+    console.error("Error:", error);
   });
--- ecommerce/src/main/resources/static/js/catTodos.js
+++ ecommerce/src/main/resources/static/js/catTodos.js
@@ -36,53 +36,65 @@ window.addEventListener("load", function () {
       contenedorTarjetas.insertAdjacentHTML("beforeend", card);
     });
   }
-  let buttons = document.querySelectorAll(".popover-btn");
-  let popovers = document.querySelectorAll(".popover");
+});
+//CAMBIAR ARREGLO DE OBJETOS POR LO QUE DEVUELVE LA PROMESA
+// Realiza la llamada al API para obtener la lista de objetos modelo
+fetch("http://127.0.0.1:8080/api/productos/")
+  .then((response) => response.json())
+  .then((products) => {
+    // Maneja la lista de productos
+    products.forEach((cargado) => {
+      let card = `
+        <div id="${cargado.id}" class="card m-3 col-sm-4 col-md-4 col-lg-3 col-xl-3 ">
+        <img src="${cargado.imagen}" alt="img" class="img-fluid fixed-image cardImage">
+          <div class="card-body">
+            <h5 class="card-title">${cargado.nombre}</h5>
+            <p class="card-price">Precio: ${cargado.precio}</p>
+            <div>
+            <div>
+            <p class="card-text">${cargado.descripcion}</p>
+            <button class="popover-btn">Leer más </button>
+            <button  onclick="agregarProducto(event)" class="btn carritoBoton"><i class="bi bi-cart-plus fs-4"></i></button>
+            <section class="popover">
+            <h3>
+            ${cargado.nombre}
+            </h3>
+            <span>
+            Precio: <strong >${cargado.precio}</strong>
+            </span>
+            <p>
+            ${cargado.descripcion}
+            </p>
+            <button onclick="eliminarPoper(event)" class="" id="btnPoper"><i class="bi bi-x-circle fs-5"></i></button>
+            </section>
+            </div>
+          </div>
+        </div>
+      `;
 
-  buttons.forEach(function (button, index) {
-    let popover = popovers[index];
-    button.addEventListener("click", function () {
-      popover.style.display = "block";
+      contenedorTarjetas.insertAdjacentHTML("beforeend", card);
     });
-  });
+    let buttons = document.querySelectorAll(".popover-btn");
+    let popovers = document.querySelectorAll(".popover");
 
-  document.addEventListener("click", function (event) {
     buttons.forEach(function (button, index) {
       let popover = popovers[index];
-      if (!button.contains(event.target) && !popover.contains(event.target)) {
-        popover.style.display = "none";
-      }
+      button.addEventListener("click", function () {
+        popover.style.display = "block";
+      });
     });
-  });
-});
-articulos.forEach((cargado) => {
-  let card = `
-    <div id="${cargado.id}" class="card m-3 col-sm-4 col-md-4 col-lg-3 col-xl-3 ">
-    <img src="${cargado.imagen}" alt="img" class="img-fluid fixed-image cardImage">
-      <div class="card-body">
-        <h5 class="card-title">${cargado.nombre}</h5>
-        <p class="card-price">Precio: ${cargado.precio}</p>
-        <div>
-        <div>
-        <p class="card-text">${cargado.descripcion}</p>
-        <button class="popover-btn">Leer más </button>
-        <button  onclick="agregarProducto(event)" class="btn carritoBoton"><i class="bi bi-cart-plus fs-4"></i></button>
-        <section class="popover">
-        <h3>
-        ${cargado.nombre}
-        </h3>
-        <span>
-        Precio: <strong >${cargado.precio}</strong>
-        </span>
-        <p>
-        ${cargado.descripcion}
-        </p>
-        <button onclick="eliminarPoper(event)" class="" id="btnPoper"><i class="bi bi-x-circle fs-5"></i></button>
-        </section>
-        </div>
-      </div>
-    </div>
-  `;
 
-  contenedorTarjetas.insertAdjacentHTML("beforeend", card);
-});
+    document.addEventListener("click", function (event) {
+      buttons.forEach(function (button, index) {
+        let popover = popovers[index];
+        if (!button.contains(event.target) && !popover.contains(event.target)) {
+          popover.style.display = "none";
+        }
+      });
+    });
+    // Realiza acciones adicionales con la lista de productos, como mostrarlos en la interfaz de usuario
+  })
+  .catch((error) => {
+    // Maneja errores de la solicitud
+    console.error("Error:", error);
+  });
